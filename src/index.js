09916965035de511ba02116db486c878d7e4d8f7
Georsav <122749052+Georsav@users.noreply.github.com>
2024-04-11 20:28:58 +0300

    home page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
+import {createHome} from './home.js';
+const homeBtn = document.getElementById('home');
+const menuBtn = document.getElementById('menu');
+const aboutBtn = document.getElementById('about');
 
 console.log("Hello");
+
+//createHome();
+
+homeBtn.addEventListener('click', function() {
+    document.getElementById('content').innerHTML = "";
+    createHome();
+});
